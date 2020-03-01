1416a829087ff593c7f66589ea732ac944ee3c27
Arthur <Debruit63@gmai.com>
2020-03-01 07:40:28 +0400

    fix: solve season

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr ) {
+    calculateDepth(arr,depth ) {
         throw 'Not implemented';
+//         depth=0;
+// let newarr = [];
+// if(newarr.length === arr.length){
+
+//     return depth;
+// }
+// else{
+//     newarr = arr.flat();
+//     return this.calculateDepth(newarr,depth++)
+}
 
     
         // remove line with error and write your code here
     }
-};
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,24 @@
-const map = new Map([
-  [0, "winter"],
-  [1, "winter"],
-  [2, "spring"],
-  [3, "spring"],
-  [4, "spring"],
-  [5, "summer"],
-  [6, "summer"],
-  [7, "summer"],
-  [8, "autumn"],
-  [9, "autumn"],
-  [10, "autumn"],
-  [11, "winter"]
-]);
+const map = [
+  "winter",
+  "winter",
+  "spring",
+  "spring",
+  "spring",
+  "summer",
+  "summer",
+  "summer",
+  "autumn",
+  "autumn",
+  "autumn",
+  "winter"
+];
 
 module.exports = function getSeason(date) {
-  if (date === undefined) return 'Unable to determine the time of year!';
+  if (date === undefined) return "Unable to determine the time of year!";
+ if (!(date instanceof Date)) {
+    throw new Error ();
+  } 
+    return map[date.getMonth()];
 
-  return map.get(date.getMonth());
   // remove line with error and write your code here
 };
