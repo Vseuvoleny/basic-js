f57629281e25adf6a24ce2aaffdf932962847e54
Arthur <Debruit63@gmai.com>
2020-10-13 23:06:01 +0400

    Feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,17 +1,85 @@
 module.exports = function repeater(str, options) {
-  let result = "";
+  let result = String(str);
   if (options) {
-    if (options.repeatTimes && !isNaN(options.repeatTimes)) {
-      for (let i = 0; i < options.repeatTimes; i++) {
-        result += str;
-      }
+    if (options.repeatTimes) {
+      result = repeatTimes(str, options.repeatTimes);
+    } else {
+      return result + options.addition;
     }
     if (options.separator) {
-      str = str + options.separator;
-    } else {
-      str = str + "+";
+      result = separator(
+        repeatTimes(str, options.repeatTimes),
+        options.separator
+      );
+    }
+    if (
+      options.addition ||
+      typeof options.addition === "boolean" ||
+      options.addition === null
+    ) {
+      result = addition(str, options);
     }
   }
-
   return result;
 };
+
+function repeatTimes(str, repeat, addition = "+") {
+  if (str === null) {
+    str = String(str);
+  }
+  const array = [];
+  let result;
+  for (let i = 0; i < repeat; i++) {
+    array.push(str);
+  }
+  if (addition === "+") {
+    result = array.join(addition);
+  } else {
+    result = array.join(addition) + addition;
+  }
+  return result;
+}
+
+function separator(str, separator, addition = "+") {
+  if (str === null) {
+    str = String(str);
+  }
+  return str.split(addition).join(separator);
+}
+
+function addition(
+  str,
+  {
+    repeatTimes,
+    separator,
+    addition,
+    additionSeparator,
+    additionRepeatTimes = 1
+  }
+) {
+  if (str === null) {
+    str = String(str);
+  }
+  const array = [];
+  let result = str;
+  let newone = "";
+  if (typeof addition === "boolean" || addition === null) {
+    addition = String(addition);
+  }
+  for (let i = 0; i < repeatTimes; i++) {
+    array.push(str);
+  }
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    if (additionSeparator) {
+      const additionwithS = addition + additionSeparator;
+      newone += additionwithS;
+    } else {
+      newone += addition;
+    }
+  }
+  newone += addition;
+  result = array.join(newone);
+  result = result.split(newone).join(newone + separator);
+
+  return result + newone;
+}
